71f530ca6c8420945f2cd20b1cb640ae2216f41b
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-20 01:43:22 -0400

    add initializer and run functions to app

--- app.go
+++ app.go
@@ -4,3 +4,22 @@ type App struct {
 	Router *mux.Router
 	DB     *sql.DB
 }
+
+func (a *App) Initialize(host string, port int, user, password, dbname string) {
+	connectionString :=
+		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+}
+
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":5000", a.Router))
+}
