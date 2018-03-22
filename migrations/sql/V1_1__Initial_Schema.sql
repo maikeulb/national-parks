31212e1fc5dd2846f5c83151ae8e3d94aa7aad9e
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-22 18:05:02 -0400

    add migration script

---
+++ migrations/sql/V1_1__Initial_Schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE states (
+    id integer PRIMARY KEY,
+    name varchar(50) NULL,
+)
+
+CREATE TABLE national_parks
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    date_designated timestamp NOT NULL,
+    state_id integer NULL
+)
+
+ALTER TABLE national_parks
+ADD CONSTRAINT fk_national_parks_states
+FOREIGN KEY (state_id)
+REFERENCES states(id)
+ON DELETE CASCADE;
