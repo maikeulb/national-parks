69dfb11d6def32816d829ecdfe95c3db468d8de2
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-22 18:24:58 -0400

    add routehandlers

--- app.go
+++ app.go
@@ -4,13 +4,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"strconv"
-	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
 )
-
 
 type App struct {
 	Router *mux.Router
@@ -37,10 +36,127 @@ func (a *App) Run(addr string) {
 }
 
 func (a *App) initializeRoutes() {
-	a.Router.HandleFunc("/api/products", a.getProducts).Methods("GET")
-	a.Router.HandleFunc("/api/product", a.createProduct).Methods("POST")
-	a.Router.HandleFunc("/api/product/{id:[0-9]+}", a.getProduct).Methods("GET")
-	a.Router.HandleFunc("/api/product/{id:[0-9]+}", a.updateProduct).Methods("PUT")
-	a.Router.HandleFunc("/api/product/{id:[0-9]+}", a.deleteProduct).Methods("DELETE")
+	a.Router.HandleFunc("/api/states", a.getStates).Methods("GET")
+	a.Router.HandleFunc("/api/states", a.createState).Methods("POST")
+	a.Router.HandleFunc("/api/states/{id:[0-9]+}", a.getState).Methods("GET")
+	a.Router.HandleFunc("/api/states/{id:[0-9]+}", a.updateState).Methods("PUT")
+	a.Router.HandleFunc("/api/states/{id:[0-9]+}", a.deleteState).Methods("DELETE")
+	a.Router.HandleFunc("/api/states/{sid}/national_parks", a.getNationalPark).Methods("GET")
+	a.Router.HandleFunc("/api/states/{sid}/national_parks", a.createNationalPark).Methods("POST")
+	a.Router.HandleFunc("/api/states/{sid}/national_parks/{id:[0-9]+}", a.getNationalPark).Methods("GET")
+	a.Router.HandleFunc("/api/states/{sid}/national_parks/{id:[0-9]+}", a.updateNationalPark).Methods("PUT")
+	a.Router.HandleFunc("/api/states/{sid}/national_parks/{id:[0-9]+}", a.deleteNationalPark).Methods("DELETE")
 }
 
+func (a *App) getStates(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+	if start < 0 {
+		start = 0
+	}
+
+	states, err := getStates(a.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, states)
+}
+
+func (a *App) createState(w http.ResponseWriter, r *http.Request) {
+	var p state
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := p.createState(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, p)
+}
+
+func (a *App) getState(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid state ID")
+		return
+	}
+
+	s := state{ID: id}
+	if err := s.getState(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "State not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, s)
+}
+
+func (a *App) updateState(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid state ID")
+		return
+	}
+
+	var s state
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&s); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+		return
+	}
+	defer r.Body.Close()
+	s.ID = id
+
+	if err := s.updateState(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func (a *App) deleteState(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid state ID")
+		return
+	}
+
+	s := state{ID: id}
+	if err := s.deleteState(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
